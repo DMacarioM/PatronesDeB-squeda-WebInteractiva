58e61698331ef8b3ae154735859e1e59e1b0ecbf
DMacarioM <dmacario5@alu.ucam.edu>
2024-05-30 11:43:34 +0200

    fix

--- client/src/pages/SandboxPageComponents/VisualComponent/KonvaHandler.jsx
+++ client/src/pages/SandboxPageComponents/VisualComponent/KonvaHandler.jsx
@@ -1,114 +1,96 @@
 import React, { useContext, useEffect, useState, useRef } from 'react';
 import { Stage, Layer,  Text, Rect,Group} from "react-konva";
 import { useLogContext } from "../../../context/useLogContext";
-import { dibujoVacio ,errorKonva, establecerDibujo , establecerDibujoInicial } from "./KonvaHandler";
 
+export const getColorFromStatus = (status) => {
+    switch (status) {
+      case 'Fallo': return '#FF0000';
+      case 'Acierto': return '#63BB66';
+      case 'EXITO': return '#9CCC66';
+      case 'NEWELEMENT': return '#85C2FF';
+      default: return 'white';
+    }
+  };
 
-const KonvaComponent = () => {
-    const { pasos, setDrawStatus, currentLogIndex } = useLogContext();
-    const [elements, setElements] = useState([]);
-    const timeoutRef = useRef(null);
-    const [lastExecutes, setLastExecutes] = useState([]); // Ahora es un array
+  //TODO: Comprobar si el paso es de tipo FuerzaBruta o no, en caso de que sí, se creará una Layer encima del actual para mostrar la tabla
+  //TODO: Controlar Tiempo de espera y TamañoTexto en variables "globales" con botones para editarlos
 
-    useEffect(() => {
-        //console.log("useEffectActual: "+ currentLogIndex);
-        //console.log("Soy : "+ pasos[currentLogIndex]?.message);
-        if(pasos[currentLogIndex]){
-            //pasos[currentLogIndex].id=currentLogIndex; //Cambiar y usar el setState o setAlgo con el contexto?¿
-            console.log("ID: "+ pasos[currentLogIndex].id);
-            if(timeoutRef.current) {
-                clearTimeout(timeoutRef.current);
-              }
+export const establecerDibujo = (paso) => {
+    //const caracteresMadre = paso.motherString.split('');
+    const caracteresPatron = paso.pattern.split('');
+    const tamanoTexto = 50;
+    const distancia = tamanoTexto +20; // Distancia entre caracteres
+    return (
+        <Group key={`${paso.id}-PAS-`}>
+            {caracteresPatron.map((pcaracter, index) => (
+              <React.Fragment key={`${pcaracter}-${index}`}>
+                {paso.posEnPatron==index && (
+                <>
+                <Rect
+                    x={(index * distancia)+ (paso.posEnCMadre*distancia)}
+                    y={paso.alturaY*distancia}
+                    width={tamanoTexto + 6}
+                    height={tamanoTexto + 7}
+                    stroke='black'
+                    fill={getColorFromStatus(paso.status)} // Aquí se establece el color de fondo
+                    strokeWidth={4} 
+                />
+                <Text
+                    x={(index * distancia + 5) + (paso.posEnCMadre*distancia)}
+                    y={paso.alturaY*distancia + 6}
+                    text={pcaracter}
+                    fontSize={tamanoTexto} />
+                </>)}
+            </React.Fragment>
+        ))}
+        </Group>
+    );
+  };
 
-            if(pasos[currentLogIndex]?.drawStatus){
-                if(pasos[currentLogIndex-1]){
-                    if(pasos[currentLogIndex-1]?.drawStatus){
-                        setElements([pasos[currentLogIndex-1].drawStatus]);
-                    }
-                }
-            }else if((pasos[currentLogIndex].status=="EXECUTE")||(pasos[currentLogIndex].status=="FIN")||(pasos[currentLogIndex].status=="RESET")){
-                pasos[currentLogIndex].id=currentLogIndex; //Cambiar y usar el setState o setAlgo con el contexto?¿
-                console.log("Id- EXEC"+pasos[currentLogIndex].id)
-                if(pasos[currentLogIndex].status=="FIN"){
-                    return;
-                    //TODO: En el dibujo final, recoger los objetos anteriores (hasta el execute incluido),
-                    //y (en caso de que el patrón.length>1) buscar paso EXITO, y si el paso anterior es acierto, cambiarlo a Exito/Crear un elemento encima (cabiar el color a Éxito) 
-                    //En caso de que no sea viable, cuando un paso sea exito, volver a hacer los aciertos anteriores con exito(Para que así el elemento paso sea exito también).
-                     //setDrawStatus(pasos[currentLogIndex], establecerDibujoFinal(pasos[currentLogIndex]))
-                }else if(pasos[currentLogIndex].status=="EXECUTE"){
-                  setDrawStatus(pasos[currentLogIndex], establecerDibujoInicial(pasos[currentLogIndex]))
-                  setLastExecutes([...lastExecutes, pasos[currentLogIndex]]); // Añadir al array
-                }else if(pasos[currentLogIndex].status=="RESET"){
-                    setDrawStatus(pasos[currentLogIndex], dibujoVacio())
-                }
-                else{return;}
-                setElements([pasos[currentLogIndex].drawStatus]);
-                return;
-            }else{
-                pasos[currentLogIndex].id=currentLogIndex; //Cambiar y usar el setState o setAlgo con el contexto?¿
-                console.log("Id- NORMAL"+pasos[currentLogIndex].id)
-                if(pasos[currentLogIndex-1]){ 
-                    if(pasos[currentLogIndex-1]?.drawStatus){
-                        if((pasos[currentLogIndex-1].status == "Fallo")||(pasos[currentLogIndex-1].status == "EXITO")) {
-                            pasos[currentLogIndex].alturaY = (pasos[currentLogIndex-1].alturaY + 1) || 1;
-                        }else{
-                            pasos[currentLogIndex].alturaY = pasos[currentLogIndex-1].alturaY || 1;
-                        }
-
-                        if(lastExecutes.length > 0) { // Si existe un paso EXECUTE
-
-                            const newElements = pasos.map(paso => {
-                                if (paso.id > lastExecutes[lastExecutes.length - 1].id && paso.id <= currentLogIndex) {
-                                    //console.log("Pasos entre lasExec");
-                                    //console.log(paso);
-                                    return establecerDibujo(paso);
-                                } else if (paso.id === lastExecutes[lastExecutes.length - 1].id) { // Si el paso actual es EXECUTE
-                                    //console.log("Last exec: "+paso.id);
-                                    //console.log(paso);
-                                    return establecerDibujoInicial(paso);
-                                } else {
-                                    return null;
-                                }
-                            });
-                            console.log("New elements: - ");
-                            console.log(newElements);
-
-                            setDrawStatus(pasos[currentLogIndex],newElements);
-                            setElements(pasos[currentLogIndex-1].drawStatus);
-                        }else{
-                         console.log("Error de Execute");   
-                        }
-                    }else{
-                        setElements(errorKonva())
-                    }
-                }else{
-                    setElements(errorKonva())
-                }
-
-            }
-
-            const tiempoRetraso = 2000; // Define tu tiempo de retraso aquí (en milisegundos)
-            timeoutRef.current = setTimeout(() => {
-                if(pasos[currentLogIndex]?.drawStatus){
-                    setElements([pasos[currentLogIndex].drawStatus]);
-                }
-            },tiempoRetraso);
-        }
-        return () => {
-        if(timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }}
-    }, [pasos,currentLogIndex]);
+  export const establecerDibujoInicial = (paso) => {
+    // Crear una lista de caracteres
+    const caracteresMadre = paso.motherString.split('');
+    //const caracteresPatron = paso.pattern.split('');
+    const tamanoTexto = 50; 
+    const distancia = tamanoTexto +20; // Distancia entre caracteres
 
     return (
-        <div>
-            <Stage width={950} height={500}>
-                <Layer x={4} y={4} draggable>
-                     {elements}
-                </Layer>
-            </Stage>
-        </div>
+          <Group key={`${paso.id}-IN-`}>
+            {caracteresMadre.map((mcaracter, index) => (
+                <React.Fragment key={`${mcaracter}-${index}`}>
+                    <Rect
+                        x={(index * distancia)}
+                        y={0}
+                        width={tamanoTexto+6}
+                        height={tamanoTexto+7}
+                        stroke='black'
+                        strokeWidth={4}
+                    />
+                    <Text
+                        x={(index * distancia) +5}
+                        y={6}
+                        text={mcaracter}
+                        fontSize={tamanoTexto}
+                    />
+                </React.Fragment>
+            ))}
+          </Group>
     );
-};
+  };
 
-export default KonvaComponent;
+  export const dibujoVacio = () => {
+    return (
+          <Group key={`-RESET-`}>
+           
+          </Group>
+    );
+  };
+  export const errorKonva = () => {
+    return (
+          <Group key={`-Error-`}>
+           <Text text='Error desconocido' />
+          </Group>
+    );
+  };
+
+  export default {dibujoVacio, errorKonva, establecerDibujoInicial,establecerDibujo };
